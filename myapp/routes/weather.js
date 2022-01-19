de480c7f76eee689fde6510ba76f068da6a646f8
edufardo <eduardo.larraz.labordeta@gmail.com>
2022-01-19 17:24:05 +0100

    WIP - APi weather

--- myapp/routes/weather.js
+++ myapp/routes/weather.js
@@ -1,26 +1,22 @@
-var express = require('express')
-var router = express.Router()
-var axios = require('axios');
-require('dotenv').config()
+var express = require("express");
+var router = express.Router();
+var axios = require("axios");
+require("dotenv").config();
 
-const climaModel = require('../models/climaModel');
+const climaModel = require("../models/climaModel");
 
-const API_key = process.env.API_key 
+const API_key = process.env.API_key;
 const excludes = "current,minutely,alerts";
 
-router.get('/', async function (req, res) {
-    let { lat, lon } = req.query
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&excludes=${excludes}&appid=${API_key}`;
-    let data = await axios.get(url).then(res => res.data)
-    let clima = new climaModel({ lat, lon })
-    try {
-        clima.save(data)
-        res.send('Guardado! ¡')
-    } catch (error) {
-        console.log(error)
-    }
-    res.send(hourly)
-})
+router.get("/", async function (req, res) {
+  let { lat, lon } = req.query;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&excludes=${excludes}&appid=${API_key}`;
+  try {
+    let data = await axios.get(url).then((response) => response.data);
+    res.send(data);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
