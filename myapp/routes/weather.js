485ca63cb693ad1cec0cbd16595848e1243543e5
edufardo <eduardo.larraz.labordeta@gmail.com>
2022-01-20 12:13:10 +0100

    UPD - Get clima model

--- myapp/routes/weather.js
+++ myapp/routes/weather.js
@@ -1,21 +1,25 @@
 var express = require("express");
 var router = express.Router();
-var axios = require("axios");
+const axios = require("axios");
 require("dotenv").config();
 
 const climaModel = require("../models/climaModel");
 
 const API_key = process.env.API_key;
-const excludes = "current,minutely,alerts";
+const exclued = "current,minutely,alerts";
 
 router.get("/", async function (req, res) {
   let { lat, lon } = req.query;
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&excludes=${excludes}&appid=${API_key}`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclued}&appid=${API_key}`;
   try {
-    let data = await axios.get(url).then((response) => response.data);
-    res.send(data);
+    let data = await axios.get(url);
+    let response = data.data;
+    let object = new climaModel(response)
+    await object.save();
+    res.send('Objeto guardado')
   } catch (error) {
-    throw new Error(error);
+    // throw new Error(error);
+    console.error("Error " + error.message);
   }
 });
 
